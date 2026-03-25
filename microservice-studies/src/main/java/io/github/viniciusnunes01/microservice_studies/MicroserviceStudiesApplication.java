9edca2ecd0f7bd8c6523fae312c04dc45b1300d7
ViniciusNunes01 <sanjip415@gmail.com>
2026-03-25 09:45:58 -0300

    first commit

---
+++ microservice-studies/src/main/java/io/github/viniciusnunes01/microservice_studies/MicroserviceStudiesApplication.java
@@ -0,0 +1,14 @@
+package io.github.viniciusnunes01.microservice_studies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceStudiesApplication.class, args);
+		System.out.println("OIEEEEE");
+	}
+
+}
---
+++ microservice-studies/src/test/java/io/github/viniciusnunes01/microservice_studies/MicroserviceStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.viniciusnunes01.microservice_studies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
