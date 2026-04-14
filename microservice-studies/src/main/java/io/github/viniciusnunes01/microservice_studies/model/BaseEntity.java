932b95784757e22c0baf05273d37b483f8b3bd8c
ViniciusNunes01 <sanjip415@gmail.com>
2026-04-14 09:26:10 -0300

    aula relacionamentos

---
+++ microservice-studies/src/main/java/io/github/viniciusnunes01/microservice_studies/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package io.github.viniciusnunes01.microservice_studies.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+@MappedSuperclass
+@Data
+public abstract class BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private LocalDateTime createdAt;
+}
